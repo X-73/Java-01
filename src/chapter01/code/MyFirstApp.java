f59799917e254f16f0cecaf3b10d40f219296721
chong <chongweizhou73@gmail.com>
2024-01-03 21:36:46 +0800

    1、使用base64格式图片替换原本的本地图片 2、增加循环、条件等java程序

---
+++ src/chapter01/code/MyFirstApp.java
@@ -0,0 +1,8 @@
+package chapter01.code;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("I Rule!");
+        System.out.println("The world");
+    }
+}
