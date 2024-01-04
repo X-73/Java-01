69e4e7095a8fe8839fb70a68fc443f58af8be8f7
chong <chongweizhou73@gmail.com>
2024-01-04 22:53:37 +0800

    开始第二章的学习回顾 1、创建md文件，不过没有填写内容 2、编写Dog、Movie两个简单代码程序

---
+++ src/chapter02/code/Dog.java
@@ -0,0 +1,11 @@
+package chapter02.code;
+
+class Dog {
+    int size;
+    String bread;
+    String name;
+
+    void bark() {
+        System.out.println("Ruff! Ruff!");
+    }
+}
---
+++ src/chapter02/code/DogTestDrive.java
@@ -0,0 +1,9 @@
+package chapter02.code;
+
+class DogTestDrive {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
