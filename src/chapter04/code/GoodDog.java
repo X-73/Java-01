ddf28ed515fea1b40d2ecb32ff8a0ebb4059f7a8
chong <chongweizhou73@gmail.com>
2024-01-09 21:53:52 +0800

    1、第4章，使用Dog、GoodDog代码，简单认识学习一波封装

---
+++ src/chapter04/code/GoodDog.java
@@ -0,0 +1,24 @@
+package chapter04.code;
+
+class GoodDog {
+    int size;
+    String name;
+
+    void bark() {
+        if (size>60){
+            System.out.println("Woof! Woof!");
+        } else if (size>14) {
+            System.out.println("Ruff! Ruff!");
+        } else {
+            System.out.println("Yip! Yip!");
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int s) {
+        this.size = s;
+    }
+}
