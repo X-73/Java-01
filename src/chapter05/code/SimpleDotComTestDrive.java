3724ee7e95f8e824eae9c826f1d4ee73141a0cf0
chong <chongweizhou73@gmail.com>
2024-01-11 22:48:38 +0800

    1、第5章，代码文件

---
+++ src/chapter05/code/SimpleDotComTestDrive.java
@@ -0,0 +1,31 @@
+package chapter05.code;
+
+import jdk.nashorn.internal.objects.annotations.Where;
+
+import javax.xml.transform.Result;
+
+public class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+        int numOfGuess = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+
+        theDotCom.setLocationCells(locations);
+
+        boolean isAlive = true;
+
+        while (isAlive == true) {
+            String guess = helper.getUserInput("enter a number");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuess++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numOfGuess + " guesses");
+            }
+        }
+    }
+}
