689f048d326338c35c2c8248e4297fab2ab9d16e
chong <chongweizhou73@gmail.com>
2024-01-13 20:58:59 +0800

    1. DotCom.java程序的优化

---
+++ src/chapter06/code/DotCom.java
@@ -0,0 +1,28 @@
+package chapter06.code;
+
+import java.util.ArrayList;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> loc) {
+        this.locationCells = loc;
+    }
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+            } else {
+                result = "hit";
+            }
+        }
+        return result;
+    }
+
+
+}
